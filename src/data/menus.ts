b9d524e786cf5888b3a3d37d99db427c39a900c0
Utsab Adhikari <48797078+autsav@users.noreply.github.com>
2025-12-23 12:36:19 +0000

    style: update mix vegetable momo image to after-crackling.png

--- src/data/menus.ts
+++ src/data/menus.ts
@@ -11,7 +11,7 @@ import vegHakka from "../assets/images/veg hakka.jpg";
 import brownie from "../assets/images/brownie.jpg";
 import rabbit from "../assets/images/rabbit.jpg";
 // Mockup imports
-import mixVegMomo from "../assets/images/mix_veg_momo.jpg";
+import mixVegMomo from "../assets/images/after-crackling.png";
 import cracklingSpinach from "../assets/images/crackling_spinach_mockup_1765787204079.png";
 import popcornCauliflower from "../assets/images/popcorn_cauliflower_mockup_1765787222926.png";
 import okraSaltPepper from "../assets/images/okra_salt_pepper_mockup_1765787251345.png";
